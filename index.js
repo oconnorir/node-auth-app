253c131465dbba530734f12fce38308b821f674c
Erin O'Connor <48839566+oconnorir@users.noreply.github.com>
2020-09-21 22:17:01 -0700

    initialized and configured app to use sessions

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 // NPM MODULES
-const express = require('express')
-const path    = require('path')
+const express         = require('express')
+const path            = require('path')
+const expressSession  = require('express-session')
+const passport        = require('passport')
+const Auth0Strategy   = require('passport-auth0')
+require('dotenv').config()
+
 
 // DEFAULT EXPRESS FUNCTION
 const app     = express()
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-app.use(express.static(path.join(__dirname, 'public')))
 
 // PORT SERVER FOR APP
 const port    = process.env.PORT || 8888
+
+// SESSION
+const session = {
+  secret: 'loxodonwarhammer',
+  cookie: {},
+  resave: false,
+  ssaveUninitialized: false
+}
+
+if (app.get('env') === 'production') {
+  //SERVE SECURE COOKIES, REQUIRES HTTPS
+  session.coolie.secure = true
+}
+
+// PASSPORT
+
+
+// PATH TO pug TEMPLATES
+app.set('views', path.join(__dirname, 'views'))
+
+// USE pug AS TEMPLATE ENGINE
+app.set('view engine', 'pug')
+
+// SERVE UP CSS, IMAGES, etc...
+app.use(express.static(path.join(__dirname, 'public')))
+
+// EXPRESS WILL USE express-session WITH session OBJECT
+app.use(expressSession(session))
+
+
 
 // ROUTES
 app.get('/', (req, res) => {
