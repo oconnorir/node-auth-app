05de212c6f45aa00e580c9bdf2825dc903f16c69
Erin O'Connor <48839566+oconnorir@users.noreply.github.com>
2020-09-23 00:15:19 -0700

    defined passport strategy and initialized passport

--- index.js
+++ index.js
@@ -18,16 +18,34 @@ const session = {
   secret: 'loxodonwarhammer',
   cookie: {},
   resave: false,
-  ssaveUninitialized: false
+  saveUninitialized: false
 }
 
 if (app.get('env') === 'production') {
   //SERVE SECURE COOKIES, REQUIRES HTTPS
-  session.coolie.secure = true
+  session.cookie.secure = true
 }
 
 // PASSPORT
-
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:8888/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile)
+  }
+)
 
 // PATH TO pug TEMPLATES
 app.set('views', path.join(__dirname, 'views'))
@@ -40,6 +58,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // EXPRESS WILL USE express-session WITH session OBJECT
 app.use(expressSession(session))
+
+passport.use(strategy)
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 
 
